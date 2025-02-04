ae213d345554ba508f07c6433cdcf372ded2e76a
kora <nurjantoktogulova13@gmail.com>
2025-02-05 01:59:12 +0600

    hw

--- bmi.py
+++ bmi.py
@@ -1,1 +1,22 @@
-heght = float(input("ввести рост: "))
+height = float(input("ввести рост: "))
+weight = float(input("ввести вес: "))
+
+height_m = height / 100
+bmi = weight / (height_m * height_m)
+
+print("твой индекс массы тела:", bmi)
+
+if bmi < 18.5:
+    print("недостаток веса")
+elif bmi < 24.9:
+    print("классный вес")
+elif bmi < 29.9:
+    print("много веса")
+elif bmi < 34.9:
+    print("ой ожирение 1 уровень")
+elif bmi < 39.9:
+    print("ой ожирение 2 уровень") 
+else:
+    print("запущенное ожирение 3 уровень")  
+    
+    
