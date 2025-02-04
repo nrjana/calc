e5d7210b6987c17401ed48a0a3cbe3dc8d8d6044
kora <nurjantoktogulova13@gmail.com>
2025-02-05 02:16:12 +0600

    угадайка

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,1 +1,13 @@
-print("Game")
+import random
+
+number = random.randint(1,100)
+while True:
+    guess = int(input("угадай число от 1 до 100!!"))
+    if guess == number:
+        print("йоо угадал!")
+        break
+    elif abs(guess - number) > 10:
+        print("горячо")
+    else:
+        print("холодно")
+        
