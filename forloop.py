352fa206138958b7d4ff828931fd5a7b6f203ffc
nurjan <nurjantoktogulova13@gmail.com>
2025-02-11 15:57:51 +0600

    news one

---
+++ forloop.py
@@ -0,0 +1,19 @@
+#sum = 0
+#for x in range(2,5,2):
+#      sum += x     
+#print(sum )
+
+#создание таблицы умножения
+#x=11
+#y=3
+#for x in range(1,11):
+#    for y in range (1,11):  
+#        print(f"{x} x {y} = {x*y}")
+#        print()
+                
+#треугольник из цифр
+for i in range(1, 11):
+    for j in range(1,1+ i):
+        print(j, end="")   
+    print()             
+        
